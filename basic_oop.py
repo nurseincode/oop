8fea33e75475ed98e70b8b4d884b56f9fa1ce175
https://github.com/nurseincode <nurseincode@gmail.com>
2025-09-25 19:58:17 +1000

    dunder in action

--- basic_oop.py
+++ basic_oop.py
@@ -14,19 +14,49 @@
 # print(my_car)
 # print(your_car)
 
+# class Car:
+#     def __init__(self, make, model):
+#         # Create an attribute 'make' & copy the value of 'make' param into it
+#         self.make = make # dot notation - <object>.<attr/method>
+#         self.model = model 
+
+    
+#     def start(self):
+#         print(f'{self.make} {self.model} started!')
+    
+#     def __str__(self):
+#         return(f'This is {self.make} {self.model}') # return a readable string describing the object
+
+
+# # Main
+# my_car = Car('Toyota', 'Prius')
+# # my_car.start()
+# your_car = Car('Ford', 'Ranger') 
+# # print(my_car.__dict__) # Useful during dev/debugging
+# print(your_car)
+# # print(my_car.display())
+# # your_car.start()
+# # print(my_car.make) # Accessing attributes, attrs not private by default
+# # print(my_car.model)
+
+
 class Car:
     def __init__(self, make, model):
-        # Create an attribute 'make' & copy the value of 'make' param into it
-        self.make = make # dot notation - <object>.<attr/method>
+        # Dunder makes the attribute private
+        self.__make = make 
         self.model = model 
+
     
     def start(self):
-        print(f'{self.make} {self.model} started!')
+        print(f'{self.__make} {self.model} started!')
+    
+    def __str__(self):
+        return(f'This is {self.__make} {self.model}')
 
 
 # Main
 my_car = Car('Toyota', 'Prius')
-my_car.start()
-your_car = Car('Ford', 'Ranger') 
-# print(my_car.__dict__) # Useful during dev/debugging
-your_car.start()
+# my_car.start()
+# your_car = Car('Ford', 'Ranger') 
+print(my_car.__make)
+
